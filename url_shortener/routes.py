3024ce2d0fff16af66da0042825a9b1b1acda352
Subratkb02 <worksubrat2002@gmail.com>
2025-01-18 23:50:11 +0530

    Adding files

--- url_shortener/routes.py
+++ url_shortener/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-short = Blueprint('short', __name__, url_prefix='/shorten')
+short = Blueprint('short', __name__, url_prefix='/shortener')
 @short.route('/')
 def index():
     return "Welcome to the shortening service"
